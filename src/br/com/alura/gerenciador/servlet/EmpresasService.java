b29d312374f29c2c072e844622189d7b21cc3735
diegovss <488729+diegovss@users.noreply.github.com>
2022-02-11 00:04:21 +0000

    adding the logic that changes the webservice response based on the type that is sent by the request

--- src/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ src/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
@@ -35,11 +36,29 @@ public class EmpresasService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Empresa> empresas = new Banco().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
+		String valor = request.getHeader("Accept");
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		if (valor.contains("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			String xml = xstream.toXML(empresas);
+			
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+		}else if(valor.endsWith("json")) {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+			
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message': 'no content }");
+		}
+		
+		
+		
+
 	}
 
 }
